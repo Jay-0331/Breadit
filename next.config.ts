737e34c3c710347391d968a2a59ffeb7024ef4e0
jay vekariya <vekariya.ja@northeastern.edu>
2024-12-10 00:31:15 -0500

    fix: updated nextjs version

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+// /** @type {import('next').NextConfig} */
+import { NextConfig } from 'next'
+
+const nextConfig = {
+	images: {
+		domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+	},
+} as NextConfig
+
+module.exports = nextConfig
